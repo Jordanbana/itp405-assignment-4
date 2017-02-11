dc73dcbfad69488d061d5e46faf364bd98b8b9ab
Jordanbana <jordanbana@gmail.com>
2017-02-11 15:43:58 -0800

    Added some front end parts

---
+++ assignmnet4/resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Twitter Clone</title>
+  </head>
+  <body>
+
+<!--   FORM  --------------->
+    <div class="container">
+        <form action="" method="post">
+          <div class="col-md-6">
+            <div class="form-group">
+                <label for="title">Tweet</label>
+                <!-- <input type="text" name="title" class="form-control" id="title"> -->
+                <textarea class="form-control" name="title" id="title" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Tweet</button>
+        </form>
+      </div>
+    </div>
+
+
+
+
+
+
+    <div class="row">
+           <div class="col-xs-12">
+             <table class="table table-striped">
+               <thead>
+                 <tr>
+                   <th>ID</th>
+                   <th>Tweet</th>
+                 </tr>
+               </thead>
+               <tbody>
+
+               </tbody>
+             </table>
+           </div>
+         </div>
+
+
+
+  </body>
+</html>
